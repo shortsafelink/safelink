189e4a956d62a15aa7663d8e2e6a4976d9436a3e
shortsafelink <40764788+shortsafelink@users.noreply.github.com>
2018-07-03 00:54:04 +0800

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -19,12 +19,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*6)+ 1);
 var xxx=null;
-if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
-if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
-if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://kompisafelinkv2.blogspot.com/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="free-sketchup-material.html"}
+if(x=="2"){xxx="free-sketchup-model.html"}
+if(x=="3"){xxx="free-sketchup-visopt.html"}
+if(x=="4"){xxx="sketchup-tutorial.html"}
+if(x=="5"){xxx="v-ray-tutorial-exterior.html"}
+if(x=="6"){xxx="v-ray-interior-tutorial.html"}a_to_ve[i].href="https://shortsafelink.blogspot.com/2018/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
